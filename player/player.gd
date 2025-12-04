c4db1e6750ab9c9d1862f08dd29b11b95245b1e6
Schiggy <hillbrecht.s@gmail.com>
2025-12-04 21:05:20 +0100

    refined player movement

--- player/player.gd
+++ player/player.gd
@@ -3,9 +3,16 @@ extends CharacterBody2D
 @export var speed: float = 70.0
 
 func _physics_process(_delta: float) -> void:
+	var input = _get_input()
+	_move(input)
+
+func _get_input():
 	var input = Vector2.ZERO
 	input.x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	input.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	input = input.normalized()
+	return input
+
+func _move(input: Vector2):
 	velocity = input * speed
 	move_and_slide()
