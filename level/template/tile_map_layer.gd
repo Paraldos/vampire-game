ada4c367937f653d0ac30d7a0b792028108e88e5
Schiggy <hillbrecht.s@gmail.com>
2025-12-14 13:34:06 +0100

    added basic setup for astar navigation

--- level/template/tile_map_layer.gd
+++ level/template/tile_map_layer.gd
@@ -1,11 +1,17 @@
 extends TileMapLayer
 
+@onready var line_2d: Line2D = $Line2D
+
 const BUILDING_SOURCE_ID: int = 3
 const BUILDING_RIM_ATLAS: Vector2i = Vector2i(0, 0)
 const BUILDING_RIM_REPLACEMENT_ATLAS: Vector2i = Vector2i(1, 0)
+const BUILDING_WALL_ATLAS = Vector2(0,1)
 
 func _ready() -> void:
+	Utils.visible_path = line_2d
+	Utils.tile_map = self
 	_adjust_building_rim()
+	_init_astar()
 
 func _adjust_building_rim() -> void:
 	var cells_to_change: Array[Vector2i] = []
@@ -23,3 +29,15 @@ func _cell_matches(cell: Vector2i, source_id: int, atlas_coords: Vector2i) -> bo
 	var cell_source_id: int = get_cell_source_id(cell)
 	var cell_atlas: Vector2i = get_cell_atlas_coords(cell)
 	return cell_source_id == source_id and cell_atlas == atlas_coords
+
+func _init_astar():
+	Utils.astar_grid = AStarGrid2D.new()
+	Utils.astar_grid.region = Rect2i(get_used_rect())
+	Utils.astar_grid.cell_size = Vector2i(16,16)
+	# Utils.astar_grid.Heuristic = AStarGrid2D.HEURISTIC_OCTILE
+	Utils.astar_grid.update()
+	for cell: Vector2i in get_used_cells():
+		if _cell_matches(cell, BUILDING_SOURCE_ID, BUILDING_WALL_ATLAS):
+			Utils.astar_grid.set_point_solid(cell)
+		if _cell_matches(cell, BUILDING_SOURCE_ID, BUILDING_RIM_ATLAS):
+			Utils.astar_grid.set_point_solid(cell)
