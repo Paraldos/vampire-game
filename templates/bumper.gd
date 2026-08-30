6504c059b8ffc2c1019e864085b75e1f67637ac2
Schiggy <hillbrecht.s@gmail.com>
2026-08-30 09:01:01 +0200

    testdummy

--- templates/bumper.gd
+++ templates/bumper.gd
@@ -1,12 +1,13 @@
-extends Area2D
+extends StaticBody2D
 class_name Bumper
 
 signal bumped
 
 func _init() -> void:
-    collision_layer = 0
-    set_collision_layer_value(3, true)
-    collision_mask = 0
+	collision_layer = 0
+	set_collision_layer_value(1, true)
+	set_collision_layer_value(3, true)
+	collision_mask = 0
 
 func bump() -> void:
-    bumped.emit()
+	bumped.emit()
