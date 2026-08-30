ae1ce474004e11e7a01fc1c16040ed8e8ff9c726
Schiggy <hillbrecht.s@gmail.com>
2026-08-30 08:45:40 +0200

    bumper setup

---
+++ templates/bumper.gd
@@ -0,0 +1,12 @@
+extends Area2D
+class_name Bumper
+
+signal bumped
+
+func _init() -> void:
+    collision_layer = 0
+    set_collision_layer_value(3, true)
+    collision_mask = 0
+
+func bump() -> void:
+    bumped.emit()
