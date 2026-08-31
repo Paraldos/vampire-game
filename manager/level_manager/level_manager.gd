97572346e72320b2a69e3a1e4e36eb7fe3e7a2bf
Schiggy <hillbrecht.s@gmail.com>
2026-08-31 19:54:41 +0200

    filestructure

---
+++ manager/level_manager/level_manager.gd
@@ -0,0 +1,11 @@
+extends Resource
+class_name LevelManager
+
+@export_storage var level : PackedScene = load("uid://h6t750kmppjs")
+@export_storage var player_position : Vector2
+
+func change_level():
+	pass
+
+func load_level():
+	pass
